41e56c9e81d807275052798eb95e72d31411aed6
Dmytro Gobosh <irulka103@ukr.net>
2021-12-16 16:37:00 +0200

    + task #8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,22 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const formElements = event.currentTarget.elements;
+  const email = formElements.email.value;
+  const password = formElements.password.value;
+
+  const objecto = {
+    email,
+    password,
+  };
+  if (email === "" || password === "") {
+    return alert("Please, fill in all appropriate fields");
+  }
+
+  console.log(objecto);
+  event.currentTarget.reset();
+}
